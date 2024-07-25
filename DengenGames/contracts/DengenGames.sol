f8eb095e099c802ac335e07f561b1309f37c3feb
Codeinx <aseyeged@gmail.com>
2024-07-25 11:57:01 +0100

    Xcel

--- DengenGames/contracts/DengenGames.sol
+++ DengenGames/contracts/DengenGames.sol
@@ -1,53 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenGames is ERC20, Ownable {
-    error INSUFFICIENT_TOKEN_BALANCE(string, uint);
+error NotEnoughXcel(string message, uint requiredAmount);
 
-    enum ItemsToRedeem {
-        Toolbox,
-        Nitro,
-        Suspensions,
-        Hydraulics
+contract XcelShoes is ERC20, Ownable {
+    enum RedeemItems { Nike, NewBalance, Adidas, Crocs }
+
+    mapping(address => string[]) public _redeemedItems;
+
+    constructor() ERC20("Xcel", "XL") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    mapping(ItemsToRedeem => uint256) public itemPrices;
-
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        itemPrices[ItemsToRedeem.Toolbox] = 10 * 1e18;
-        itemPrices[ItemsToRedeem.Nitro] = 8 * 1e18;
-        itemPrices[ItemsToRedeem.Suspensions] = 6 * 1e18;
-        itemPrices[ItemsToRedeem.Hydraulics] = 4 * 1e18;
+    function transferXcel(address to, uint256 amount) external returns (bool success) {
+        if (amount > balanceOf(msg.sender)) {
+            revert NotEnoughXcel("Xcel Token not enough", amount);
+        }
+        success = transfer(to, amount);
     }
 
-    function mint(address _receiver, uint256 _amount) external onlyOwner {
-        _mint(_receiver, _amount);
+    function burn(uint256 amount) external {
+        if (amount > balanceOf(msg.sender)) {
+            revert NotEnoughXcel("Xcel Token not enough", amount);
+        }
+        _burn(msg.sender, amount);
     }
 
-    function transferDGNToken(
-        address _receiver,
-        uint256 _amount
-    ) external returns (bool success) {
-        require(_amount <= balanceOf(msg.sender), "Insufficient DGN Token");
-        return transfer(_receiver, _amount);
-    }
+    function redeemItems(RedeemItems item) external {
+        uint256 price = 0;
+        if (item == RedeemItems.Nike) {
+            price = 4 * 1e18;
+        } else if (item == RedeemItems.NewBalance) {
+            price = 3 * 1e18;
+        } else if (item == RedeemItems.Adidas) {
+            price = 2 * 1e18;
+        } else if (item == RedeemItems.Crocs) {
+            price = 1 * 1e18;
+        } else {
+            price = 0;
+        }
 
-    function burn(uint256 _amount) external {
-        require(_amount <= balanceOf(msg.sender), "Insufficient DGN Token");
-        _burn(msg.sender, _amount);
-    }
+        if (price > balanceOf(msg.sender)) {
+            revert NotEnoughXcel("Xcel Token not enough", price);
+        }
+        _transfer(msg.sender, address(this), price);
 
-    function redeemItems(ItemsToRedeem _item) external {
-        require(itemPrices[_item] > 0, "Invalid item");
-        require(
-            itemPrices[_item] <= balanceOf(msg.sender),
-            "Insufficient DGN  Token"
-        );
-
-        _transfer(msg.sender, address(this), itemPrices[_item]);
+        string[] storage currentRedeemedItems = _redeemedItems[msg.sender];
+        currentRedeemedItems.push(_itemToString(item));
     }
 
     function withdrawFunds() external onlyOwner {
@@ -56,5 +60,23 @@ contract DegenGames is ERC20, Ownable {
 
     function getBalance() public view returns (uint256) {
         return balanceOf(msg.sender);
+    }
+
+    function _itemToString(RedeemItems item) internal pure returns (string memory) {
+        if (item == RedeemItems.Nike) {
+            return "Nike";
+        } else if (item == RedeemItems.NewBalance) {
+            return "NewBalance";
+        } else if (item == RedeemItems.Adidas) {
+            return "Adidas";
+        } else if (item == RedeemItems.Crocs) {
+            return "Crocs";
+        } else {
+            return "";
+        }
+    }
+
+    function displayRedeemedItems(address user) public view returns (string[] memory) {
+        return _redeemedItems[user];
     }
 }
